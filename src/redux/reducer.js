2b4716ed4dde16aa0e3ac4b5da6f82923bc2e394
moona8 <73126379+moona8@users.noreply.github.com>
2021-11-26 20:14:20 +0500

    quiz app

---
+++ src/redux/reducer.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import todo from "../components/todo";
+
+export const todoSlice = createSlice({
+  name: "quiz",
+  initialState: {
+    todos: [],
+  },
+
+  reducers: {
+    addTodo: (state, action) => {
+        console.log(action)
+      state.todos.push(action.payload)
+    },
+    deleteTodo: (state, action) => {
+        // const {recordIndex} = action.payload;
+        state.todos.splice(action.payload,1)
+      },
+      deleteAll:(state)=>{
+          state.todos=[]
+        console.log(state)
+      },
+      editTodo:(state,action)=>{
+        state.todos[action.payload].editMode=true
+      },
+      saveTodo:(state,action)=>{
+        state.todos[action.payload.todoIndex].editMode=false
+        state.todos[action.payload.todoIndex].val=action.payload.value
+      }
+  },
+});
+
+
+export const { addTodo,deleteTodo ,deleteAll,editTodo,saveTodo} = todoSlice.actions;
+export default todoSlice.reducer;
